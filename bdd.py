74d31bb35e706a0035906f824f6b7e6d9f99f155
ad-44 <antoine.debille@gmail.com>
2025-02-20 17:10:29 +0100

    Update bdd.py

--- bdd.py
+++ bdd.py
@@ -1,12 +1,10 @@
 #%% Imports
 import pandas as pd
-import request
-
-url = "https://github.com/ad-44/LCOS_Calculator/blob/main/bdd/spotprice.csv"
-resp = requests.get(url)
 
 #%% Data base creation
-rawprices = pd.read_csv(resp,sep=';',header=0,index_col=0)
+url = (r'https://raw.githubusercontent.com/ad-44/LCOS_Calculator/main/bdd/spotprice.csv')
+
+rawprices = pd.read_csv(url,sep=';',header=0,index_col=0)
 rawprices.index = range(0,8784)
 rawprices = rawprices.astype('float')
 rawprices = rawprices.fillna(0)
