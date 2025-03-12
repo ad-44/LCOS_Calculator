cb9be2f4c7ec96d80e52e7535453b740632a48c2
ad-44 <antoine.debille@gmail.com>
2025-03-12 09:58:32 +0100

    Update bdd.py
    Grand'Maison database added

--- bdd.py
+++ bdd.py
@@ -2,9 +2,14 @@
 import pandas as pd
 
 #%% Data base creation
-url = (r'https://raw.githubusercontent.com/ad-44/LCOS_Calculator/main/bdd/spotprice.csv')
+url_spot = (r'https://raw.githubusercontent.com/ad-44/LCOS_Calculator/main/bdd/spotprice.csv')
 
-rawprices = pd.read_csv(url,sep=';',header=0,index_col=0)
+rawprices = pd.read_csv(url_spot, sep=';', header=0, index_col=0)
 rawprices.index = range(0,8784)
 rawprices = rawprices.astype('float')
 rawprices = rawprices.fillna(0)
+
+url_gm = (r'https://raw.githubusercontent.com/ad-44/LCOS_Calculator/main/bdd/GM_data.csv')
+
+rawGM = pd.read_csv(url_gm, sep=';', header=0, index_col=0)
+rawGM = rawGM.fillna(0)
